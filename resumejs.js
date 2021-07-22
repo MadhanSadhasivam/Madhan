0993b789181467de1e372c96d81fa072c25d6a81
MadhanSadhasivam <87658775+MadhanSadhasivam@users.noreply.github.com>
2021-07-22 23:19:22 +0530

    Add files via upload

---
+++ resumejs.js
@@ -0,0 +1,31 @@
+var resume = {
+
+    details:
+    {
+        name       :"Madhan A S",
+        email      :"a.s.madhan001@gmail.com",
+        phonenumber:"9095169233",
+        degree     :"B.Tech Information Technology,VFX",
+        address    :"220,Aruvankattur,Gobichettipalayam,Erode",
+        pincode    :"638476"
+        
+    },
+    academicqualification:
+    {
+        school     :"Both 10th and 12th in Sri venkateshwara vidhyala matriculation and higher secondary school in the year 2011 and 2013",
+        college    :"Velalar college of engineering and techonology graduated in 2017 and VFX in Pixelloid studios bangalore in 2021"
+    },
+    technicalqualification:
+    {
+        javascript :"Basics of javascript"
+    },
+    languages:
+    {
+        language   :"Tamil and English"
+    }, 
+    interest:
+    {
+        category   :"Interested in Farming and looking forward to learn Programming"
+    }
+}
+console.log(resume);
